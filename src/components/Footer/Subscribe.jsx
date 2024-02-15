ee4dd3ab347a1c0712857f38424d11daec782399
merndevreza <merndevreza@gmail.com>
2024-02-15 07:29:25 +0600

    Added a Subscring Modal with react-tostify alert

--- src/components/Footer/Subscribe.jsx
+++ src/components/Footer/Subscribe.jsx
@@ -1,23 +1,38 @@
+import { useState } from "react";
+import SubscribeModal from "./SubscribeModal";
+import { ToastContainer } from "react-toastify";
 
 const Subscribe = () => {
-   return (
-      
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
       <div className="col-span-12 mt-10 flex items-start gap-4 xl:col-span-5 xl:mt-0">
-      <div className="flex-1">
-        <h3 className="text-2xl font-semibold">
-          Subscribe and be informed first hand about the actual economic
-          news.
-        </h3>
-        <p className="mt-2 text-sm leading-6">
-          All the days headlines and highlights, direct to you every
-          morning..
-        </p>
+        <div className="flex-1">
+          <h3 className="text-2xl font-semibold">
+            Subscribe and be informed first hand about the actual economic news.
+          </h3>
+          <p className="mt-2 text-sm leading-6">
+            All the days headlines and highlights, direct to you every morning..
+          </p>
+        </div>
+        <button
+          className="rounded-full bg-[#00D991] px-7 py-2.5 text-xs font-medium text-[#F1EFEA] hover:opacity-80 lg:text-base"
+          onClick={() => setIsOpen(true)}
+        >
+          Subscribe
+        </button>
       </div>
-      <button className="rounded-full bg-[#00D991] px-7 py-2.5 text-xs font-medium text-[#F1EFEA] hover:opacity-80 lg:text-base">
-        Subscribe
-      </button>
-    </div>
-   );
+      {isOpen && (
+        <div className="fixed w-full h-screen left-0 top-0 bg-black bg-opacity-40">
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-5">
+            <SubscribeModal setIsOpen={setIsOpen} />
+          </div>
+        </div>
+      )}
+
+      <ToastContainer position="bottom-right" theme="dark" />
+    </>
+  );
 };
 
 export default Subscribe;
---
+++ src/components/Footer/SubscribeModal.jsx
@@ -0,0 +1,39 @@
+import logo from"../../assets/logo.png"
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const SubscribeModal = ({ setIsOpen }) => {
+   const handleSubscribe=(e)=>{
+      e.preventDefault();
+      const fullName=e.target.fullName.value;
+      const email=e.target.email.value;
+      //We can store the user info in the server through calling it's API here, in production.
+      console.log(fullName,email);
+      setIsOpen(false);
+      toast("Thanks for Subscribing our newsletter.",{
+         position: "bottom-right", 
+         theme: "dark",
+         })
+   }
+  return (
+    <div className="text-center">
+      <img className="mx-auto" src={logo} alt="Learn With Sumit Logo" />
+      <h2 className="mb-2.5 text-xl font-bold lg:text-[20px] mt-5 text-black inline-block max-w-lg">
+        Subscribe and be informed first hand about the actual economic news.
+      </h2>
+      <form className="flex flex-col" onSubmit={(e)=>handleSubscribe(e)}>
+        <input className="p-2 bg-slate-100 text-black mb-4" type="text" required name="fullName" placeholder="Full name" />
+        <input className="p-2 bg-slate-100 text-black mb-4" type="email" required name="email" placeholder="Email" />
+        <input className="bg-primary py-2 mb-3 rounded cursor-pointer hover:bg-[rgb(8,124,86)]" type="submit" value="Submit" />
+
+        <button
+          onClick={() => setIsOpen(false)}
+          className="px-4 py-2 bg-red-400 hover:bg-red-500  rounded"
+        >
+          Close
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SubscribeModal;
